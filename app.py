897c62dc6dbc23283f4187bcdb8b42094a16537b
Deepaksachora <58966633+Deepaksachora@users.noreply.github.com>
2020-06-19 21:50:12 +0530

    Update app.py
    Update new code.

--- app.py
+++ app.py
@@ -1,37 +1,11 @@
-from flask import Flask, render_template, url_for, request
-from flask_socketio import SocketIO, send
-
-
-
-app = Flask(__name__, template_folder="file")
-app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app)
+from flask import render_template
+from web_sockets import app, socketio
 
 
 @app.route("/")
-
-@app.route("/index.html")
-
+@app.route("/game.html")
 def home():
-    return render_template('index.html')
-
-@app.route("/login.html")
-def login():
-    return render_template('login.html')
-
-@app.route("/join.html")
-def join():
-    return render_template('join.html')
-
-@socketio.on('game_no')
-def handle_my_custom_event(data):
-    send(data, broadcast=True)
-
-@socketio.on('message')
-def handleMessage(msg):
-    print('Message:' + msg)
-    send(msg, broadcast=True)
-
+    return render_template('game.html')
 
 if (__name__ == "__main__"):
     socketio.run(app)
